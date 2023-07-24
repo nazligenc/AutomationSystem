cbb758a89ec04e20475674b15b5ed002e9a91f43
NAZLI GENG <1910206061@ogrenci.karabuk.edu.tr>
2023-07-24 23:28:18 +0300

    butondan karşılaştırmalı veri alma

--- RM/Form2.cs
+++ RM/Form2.cs
@@ -18,6 +18,7 @@ using System.Net.NetworkInformation;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 using Button = System.Windows.Forms.Button;
 using System.Diagnostics.Eventing.Reader;
+using Microsoft.IdentityModel.Logging;
 
 namespace RM
 {
@@ -34,7 +35,7 @@ namespace RM
             buttons = new List<Button>() { button2, button3, button4 };
 
         }
-        
+
 
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -95,8 +96,10 @@ namespace RM
             //parametreli method oluştur.
 
             object a = button2.Tag;
-            ButonYazdırma(button2.Tag);
-            
+            Butonsayac(button2.Tag);
+           
+           
+
 
 
 
@@ -108,91 +111,98 @@ namespace RM
         {
 
         }
-        public void Butonsayac() 
+        
+        private HashSet<string> uniqueProductNames = new HashSet<string>(); //Hashset sınıfı elemanın sadece bir kez saklanmasına izin vermek için kullanıldı.
+        private Dictionary<Button, int> buttonClickCounts = new Dictionary<Button, int>();
+       
+    
+        public void Butonsayac(object y)
         {
+            int count = 0;
             
+            Button buttons = y as Button;
+
+            SqlConnection conn = new SqlConnection("Data Source=NAZLI\\MSSQLSERVER01;Initial Catalog=RM;Integrated Security=True");
+            conn.Open();
+            string qry = ($"SELECT urunadi,urunfiyati,urunadedi FROM operation WHERE urunID={y}");
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            cmd.Parameters.AddWithValue("@ProductId", y);
+            SqlDataReader reader = cmd.ExecuteReader();
+
             
-            
-            string[] isimler = { "çay", "kola", "cips", "haydari", "limonata", "sosisli", "çay", "kola", "fanta", "sosisli" };
-            Dictionary<string, int> sayac = new Dictionary<string, int>();
-            foreach(string eleman in isimler)
+            if (reader.Read())
             {
-                if (sayac.ContainsKey(eleman))
+                count = Convert.ToInt32(reader["urunadedi"]);
+                string urunfiyati = reader["urunfiyati"].ToString();
+                string urunAdi = reader["urunadi"].ToString();
+                if (uniqueProductNames.Contains(urunAdi))  //ürün adını arıyor hashset sınıfında varsa boş return ediyor yoksa yazdırıyor.
                 {
-                    sayac[eleman]++;
+                    return;
+
                 }
-                else
-                {
-                    sayac[eleman] =1;
-                }
-                foreach(var girdi in sayac)
-                {
-                    Console.WriteLine(girdi.Key +" sayısı " + girdi.Value +" "+" kez tekrar edildi");
-                }
+                
+                
+               
+
+                
+                int newProductCount = count + 1;
+                string updateQuery = "UPDATE operation SET urunadedi = @NewProductCount WHERE urunID = @ProductId";
+                SqlCommand updateCmd = new SqlCommand(updateQuery, conn);
+                updateCmd.Parameters.AddWithValue("@NewProductCount", newProductCount);
+                updateCmd.Parameters.AddWithValue("@ProductId",y);
+             
+                int urunadedi=Convert.ToInt32(reader["urunadedi"]);
+                decimal urunFiyati = Convert.ToDecimal(reader["urunfiyati"]);
+                decimal newtotal = 0;
+                decimal total;
+                total =newProductCount *Convert.ToDecimal(reader["urunfiyati"]);
+                newtotal += total;
+                // Butonun ürün adedini güncelledikten sonra ekrana yazdırıyoruz.
+
+
+
+
+                // Ürün adını benzersiz küme içine ekleyip bilgileri görüntülüyoruz.
+                uniqueProductNames.Add(urunAdi);
+                listBox1.Items.Add(urunAdi+    "                "   +newProductCount+    "        "   +urunfiyati+"TL");
+               label1.Text = newtotal.ToString();
+
 
             }
-            //for (s = 0; s < isimler.Length; s++)
-            //{
-            //    for (k = 0; k < isimler.Length; k++)
-            //    {
-            //        if (String.Compare(isimler[s], isimler[k]) == 0)
-            //        {
-            //            for (a = 0; a < isimler.Length; a++)
-            //            {
-            //                if (String.Compare(isimler[a], isimler[s]) == 0)
-            //                {
-                                
-            //                }
+        }
 
-            //            }
-            //        }
+       
+    
+
+    
 
 
-                   
+         
 
-            //    }
-            }
-            
+                    
+                
+                    
+                
+        
+        
 
 
            
 
         
-        public void ButonYazdırma(object x)
-        {
-            Butonsayac();
-           
-            Button buttons = x as Button;
-
-            SqlConnection conn = new SqlConnection("Data Source=NAZLI\\MSSQLSERVER01;Initial Catalog=RM;Integrated Security=True");
-            conn.Open();
-            string qry = ($"SELECT urunadi,urunadedi,urunfiyati FROM operation WHERE urunID={x}");
-            SqlCommand cmd = new SqlCommand(qry, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            //ListBox'a alt alta veri yazdırma
-            if (reader.Read())
-            {
-                
-                String a2 = "              " + reader["urunadi"] + "                 " + "                " + "                  " + reader["urunfiyati"];
-                listBox1.Items.Add(a2);
-                
+      
+            
 
 
-
-            }
-           
-            reader.Close();
-
-
-        }
+        
 
 
 
         private void button3_Click(object sender, EventArgs e)
         {
             object c = button3.Tag;
-            ButonYazdırma(button3.Tag);
+            
+            Butonsayac(button3.Tag);
             
         }
 
@@ -204,8 +214,9 @@ namespace RM
         private void button4_Click(object sender, EventArgs e)
         {
             comboBox1_SelectedIndexChanged(button4, null);
+           
             object c = button4.Tag;
-            ButonYazdırma(button4.Tag);
+            Butonsayac(button4.Tag);
             
 
         }
@@ -271,56 +282,16 @@ namespace RM
 
 
 
-            //foreach (Button button in buttons)
-            //{
-
-            //    //string a3 = button.Tag.ToString();
-            //    //string qry = ($"SELECT uruntipi FROM operation WHERE urunID={a3}");
-            //    //SqlCommand cmd = new SqlCommand(qry, conn);
-            //    //SqlDataReader reader = cmd.ExecuteReader();
-            //    //Console.WriteLine(a3);
-
-            //    if (reader.HasRows==true)
-            //    {
-            //        reader.Close();  
-            //        if (comboBox1.SelectedIndex == 0)
-            //        {
-            //            string uruns = comboBox1.SelectedText;
-            //            string qry2 = ($"SELECT uruntipi FROM operation WHERE urunID={a3}");
-            //            SqlCommand cmd2 = new SqlCommand(qry2, conn);
-            //            SqlDataReader reader2 = cmd2.ExecuteReader();
-            //            Console.WriteLine((string)reader2["uruntipi"]);
-            //        }
-            //        string c1 = "Yiyecek";
-            //        object cx = (object) c1;
-            //        //    string ac = (string)reader["uruntipi"];
-            //       // Console.WriteLine(ac);
-            //        if (reader["uruntipi"]==cx)
-            //        {
-
-            //                button.Visible = true;
-            //               Console.WriteLine("okuyor");
-
-            //        }
-            //        else
-
-            //        {
-            //            Console.WriteLine(comboBox1.SelectedIndex.GetType());
-
-            //            Console.WriteLine(comboBox1.SelectedIndex);
-            //            button.Visible = false;
-            //            Console.WriteLine("else okuyor");
-
-            //    }
-            //}
-
-            //reader.Close(); 
+           
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
+           
             object c = button8.Tag;
-            ButonYazdırma(button8.Tag);
+            
+            Butonsayac(button8.Tag);
+           
             
 
         }
@@ -348,24 +319,7 @@ namespace RM
 
 
 
-            //  string a=button2.Tag.ToString();
-
-
-
-            //string a2 = reader["uruntipi"].ToString();
-
-            // if (comboBox1.SelectedIndex == 0)
-            //{
-            //    if (a == "YİYECEK")
-            //    {
-            //        button2.Visible = true;
-            //    }
-            //    else
-            //    {
-            //        button2.Visible = false;
-            //    }
-
-            //}
+           
         }
 
         private void label1_Click_1(object sender, EventArgs e)
